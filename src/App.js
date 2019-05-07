7e440305ec16c3fc87c1155dd28dc62994bc04d5
Bruno <bpiraja97@gmail.com>
2019-05-06 21:38:51 -0300

    Material UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
 
-function App() {
+const styles = theme => ({
+  div_container: {
+    width: "auto",
+    display: "block",
+    backgroundColor: "#D3D3D3",
+    borderRadius: "3px",
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    },
+    fontSize: "14px"
+  },
+  form: {
+    margin: "auto",
+    width: "50%",
+    padding: "10px"
+  }
+});
+
+function App(props) {
+  const { classes } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className={classes.div_container}>
+      <Grid container justify="center" alignItems="center">
+        <Avatar
+          alt="Bruno Frigeri"
+          style={{ margin: "10px", backgroundColor: "#d3d" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          BF
+        </Avatar>
+      </Grid>
+
+      <form className={classes.form}>
+        <FormControl margin="normal" required fullWidth>
+          <InputLabel htmlFor="name">Name</InputLabel>
+          <Input id="name" type="text" />
+        </FormControl>
+
+        <FormControl margin="normal" required fullWidth>
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <Input id="email" type="email" />
+        </FormControl>
+
+        <FormControl margin="normal" required fullWidth>
+          <InputLabel htmlFor="email">Rewrite your email</InputLabel>
+          <Input id="email" type="email" />
+        </FormControl>
+
+        <FormControl margin="normal" required fullWidth>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <Input id="password" type="password" />
+        </FormControl>
+
+        <FormControl margin="normal" required fullWidth>
+          <InputLabel htmlFor="password">Rewrite your password</InputLabel>
+          <Input id="password" type="password" />
+        </FormControl>
+      </form>
+    </main>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
